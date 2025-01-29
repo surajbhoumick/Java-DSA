fc2e895a14f30d15e23ab159ca7d5c24a3805562
Suraj Bhoumick <131149624+surajbhoumick@users.noreply.github.com>
2025-01-29 07:08:03 +0000

    Initial

---
+++ 01_Basics/Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 30;
+        int result = num1 + num2;
+        float result_try = num1 + num2;
+
+        System.out.println(result);
+
+        System.out.println(result_try);
+    }
+}
