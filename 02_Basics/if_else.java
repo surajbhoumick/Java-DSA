06d143f9c7da9e0aac38f656f7631e62ef3cb1f6
Suraj Bhoumick <131149624+surajbhoumick@users.noreply.github.com>
2025-01-30 09:35:12 +0000

    Basics

---
+++ 02_Basics/if_else.java
@@ -0,0 +1,13 @@
+public class if_else {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 80;
+        int z = 120;
+
+        if (y > x && z > y) {
+            System.out.println("Thank You");
+        } else {
+            System.out.println("Bye");
+        }
+    }
+}
