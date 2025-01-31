867aad1e93452b05057184eef499a1d66827183f
Suraj Bhoumick <131149624+surajbhoumick@users.noreply.github.com>
2025-01-31 16:34:46 +0000

    Loops

---
+++ 03_Basics/while_loop.java
@@ -0,0 +1,12 @@
+public class while_loop {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 4) {
+            System.out.println("Hii" + i);
+            i++;
+
+        }
+
+    }
+}
