9facdf31f06bdacf1af0f229c1cff959609ae1f9
Suraj Bhoumick <131149624+surajbhoumick@users.noreply.github.com>
2025-01-31 16:37:23 +0000

    Loops

--- 03_Basics/while_loop.java
+++ 03_Basics/while_loop.java
@@ -1,12 +1,11 @@
-public class while_loop {
-    public static void main(String[] args) {
 
+public class while_loop {
+
+    public static void main(String[] args) {
         int i = 1;
         while (i <= 4) {
-            System.out.println("Hii" + i);
+            System.out.println("Hii " + i); // Added space after "Hii"
             i++;
-
         }
-
     }
 }
