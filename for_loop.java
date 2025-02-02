0f37e8378c043d7060ab1e9c108f116b3c4ec4f8
surajbhoumick <surajbhoumick360@gmail.com>
2025-02-02 23:56:58 +0530

    Loops

--- for_loop.java
+++ for_loop.java
@@ -1,6 +1,7 @@
 public class for_loop {
     public static void main(String[] args) {
-
+        // Similar to while loop
+        // Conditions are fitted into one line
         for (int i = 1; i <= 4; i++) {
             System.out.println("Hi " + i);
         }
