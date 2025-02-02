131e156d0828ebdfaacba86f69b6bd96bd4b8905
surajbhoumick <surajbhoumick360@gmail.com>
2025-02-02 23:56:29 +0530

    Loops

---
+++ do_while_loop.java
@@ -0,0 +1,10 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+
+        int i = 1;       //Do while is used to print the statement atleast one without running in loop
+        do {
+            System.out.println("Hii " + i);
+            i++;
+        } while (i <= 4);
+    }
+}
---
+++ for_loop.java
@@ -0,0 +1,8 @@
+public class for_loop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Hi " + i);
+        }
+    }
+}
