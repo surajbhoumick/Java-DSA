ad143a257b1f9ba93469093a898f1e5b4f188009
surajbhoumick <surajbhoumick360@gmail.com>
2025-02-03 00:38:56 +0530

    Try

---
+++ 03_Basics/try_loop.java
@@ -0,0 +1,9 @@
+public class try_loop {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i<=20) {
+            System.out.println("SUN " + i);
+            i++ ;
+        }
+    }
+}
