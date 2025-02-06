1ad2b86f9f6294424925865fecefadbbcb9cacaf
surajbhoumick <surajbhoumick360@gmail.com>
2025-02-06 08:19:02 +0530

    object

---
+++ 04_Basics/object.java
@@ -0,0 +1,24 @@
+class Car {
+
+    int num;
+    String word;
+
+    // Method
+    void drive() {
+        System.out.println("The Car is driving: " + word);
+    }
+}
+
+public class object {
+    public static void main(String args[]) {
+        // Create an object of Car
+        Car info = new Car();
+
+        // Set the properties for this object
+        info.num = 20;
+        info.word = "Toyota";
+
+        // Call the method to make the car drive
+        info.drive();
+    }
+}
